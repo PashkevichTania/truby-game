7b3342623c592bc44a0b8df74837ef9534614a1d
Tania <pashkevichtania2004@gmail.com>
2026-03-15 16:38:01 +0300

    add paths

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/truby-game/',
   plugins: [svgr({
     include: "**/*.svg?react",
   }),react(),tailwindcss()],
